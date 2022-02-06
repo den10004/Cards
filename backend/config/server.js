2925e1ba474558b368b2288c88c07c4975a8ae80
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-02-06 09:10:45 +0300

    начало

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '1c7e1de4d679257dce9b4f6ee18b4bf1'),
-    },
-  },
-});
--- backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '01766c5f-c516-4df6-8713-166c44d959ea'
-};
