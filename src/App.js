d7190f80a35bd0578ff083341dbee6d557b79c89
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-02-22 09:31:44 +0300

    сделан UI

--- src/App.js
+++ src/App.js
@@ -1,77 +1,98 @@
-import "./App.css";
-import Card from "./Card";
-import ContentBlock from "./ContentBlock/ContentBlock";
-import FooterBlock from "./FooterBlock/FooterBlock";
-import HeaderBlock from "./HeaderBlock";
+import React from 'react';
 
+import { ClockCircleOutlined, HomeOutlined, SmileOutlined } from '@ant-design/icons';
+
+import BackgroundBlock from './components/BackgroundBlock';
+import Button from './components/Button';
+import CardList from './components/CardList';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Paragraph from './components/Paragraph';
+import Section from './components/Section';
+
+import { wordsList } from './wordsList';
+
+import firstBackground from './assets/background.jpg';
+import secondBackground from './assets/back2.jpg';
+
+import s from './App.module.scss';
 
 
 function App() {
- const wordsList = [
-    {
-        eng: 'between',
-        rus: 'между'
-    },
-    {
-        eng: 'high',
-        rus: 'высокий'
-    },
-    {
-        eng: 'really',
-        rus: 'действительно'
-    },
-    {
-        eng: 'something',
-        rus: 'что-нибудь'
-    },
-    {
-        eng: 'most',
-        rus: 'большинство'
-    },
-    {
-        eng: 'another',
-        rus: 'другой'
-    },
-    {
-        eng: 'much',
-        rus: 'много'
-    },
-    {
-        eng: 'family',
-        rus: 'семья'
-    },
-    {
-        eng: 'own',
-        rus: 'личный'
-    },
-    {
-        eng: 'out',
-        rus: 'из/вне'
-    },
-    {
-        eng: 'leave',
-        rus: 'покидать'
-    },
-];
+    return (
+        <>
+            <BackgroundBlock
+                backgroundImg={firstBackground}
+                fullHeight
+            >
+                <Header white>
+                    Время учить слова онлайн
+                </Header>
+                <Paragraph white>
+                    Используйте карточки для запоминания и пополняйте активный словарный запас.
+                </Paragraph>
+            </BackgroundBlock>
+            <Section className={s.textCenter}>
+                <Header size="l">
+                    Мы создали уроки, чтобы помочь вам увереннее разговаривать на английском языке
+                </Header>
+                <div className={s.motivation}>
+                    <div className={s.motivationBlock}>
+                        <div className={s.icons}>
+                            <ClockCircleOutlined /> 
+                        </div>
+                        <Paragraph small>
+                            Учитесь, когда есть свободная минутка
+                        </Paragraph>
+                    </div>
 
+                    <div className={s.motivationBlock}>
+                        <div className={s.icons}>
+                            <HomeOutlined />
+                        </div>
+                        <Paragraph small>
+                            Откуда угодно — дома, в&nbsp;офисе, в&nbsp;кафе
+                        </Paragraph>
+                    </div>
 
+                    <div className={s.motivationBlock}>
+                        <div className={s.icons}>
+                            <SmileOutlined />
+                        </div>
+                        <Paragraph small>
+                            Разговоры по-английски без&nbsp;неловких пауз и&nbsp;«mmm,&nbsp;how&nbsp;to&nbsp;say…»
+                        </Paragraph>
+                    </div>
+                </div>
+            </Section>
+            <Section bgColor="#f0f0f0" className={s.textCenter}>
+                <Header size='l'>
+                    Начать учить английский просто
+                </Header>
+                <Paragraph>
+                    Клика по карточкам и узнавай новые слова, быстро и легко!
+                </Paragraph>
 
-  return (
-    <div className="App">
-      <HeaderBlock 
-      title="Учите слова"
-      descr="Воспользуйтесь карточками для запоминания и пополнения активныйх
-      словарных запасов"
-      />
-     {
-       wordsList.map(({eng, rus}, index) => 
-       <Card key={index} eng={eng} rus={rus} />)
-     }
-      <ContentBlock />
-      <FooterBlock />
-
-    </div>
-  );
+                <CardList 
+                    items={wordsList}
+                />
+            </Section>
+            <BackgroundBlock
+                backgroundImg={secondBackground}
+            >
+                <Header size="l" white>
+                    Изучайте английский с персональным сайтом помощником
+                </Header>
+                <Paragraph white>
+                    Начните прямо сейчас
+                </Paragraph>
+                <Button>
+                    Начать бесплатный урок
+                </Button>
+            </BackgroundBlock>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Footer.module.scss';
+
+const Footer = () => {
+    return (
+        <footer className={s.root}>
+            Made by Denis Sergeev
+        </footer>
+    );
+};
+
+export default Footer;
